e3bfd6d83e22d62a68fd031c01a738e9003a0116
Tebas Martínez <203688711+TebasMartinez@users.noreply.github.com>
2025-08-19 18:09:08 +0200

    Update exploration.sql

--- exploration.sql
+++ exploration.sql
@@ -34,3 +34,34 @@ SELECT preferred_category,
     AVG(last_purchase_amount)
 FROM customer_segmentation
 GROUP BY preferred_category;
+
+# Customer segmentation -> create views by gender
+CREATE VIEW female_gender_cust AS
+	SELECT *
+    FROM customer_segmentation
+    WHERE gender = "female";
+    
+CREATE VIEW male_gender_cust AS
+	SELECT *
+    FROM customer_segmentation
+    WHERE gender = "male";
+    
+CREATE VIEW other_gender_cust AS
+	SELECT *
+    FROM customer_segmentation
+    WHERE gender = "other";
+    
+# Customer segmentation -> create views by spending_score, creating four customer categories based on the spending score:
+# Bad customers <- 0-25 spending score
+# Medium customers <- 26-50 spending score
+# Good customers <- 51-75 spending score
+# Great customers <- 76-100 spending score
+
+SELECT *,
+	CASE
+		WHEN spending_score > 0 AND spending_score < 26 THEN "Bad customer"
+        WHEN spending_score >= 26 AND spending_score < 51 THEN "Medium customer"
+        WHEN spending_score >= 51 AND spending_score < 76 THEN "Good customer"
+        WHEN spending_score >=76 THEN "Great customer"
+	END AS customer_category
+FROM customer_segmentation;
